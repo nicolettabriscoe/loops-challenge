b519338968feb8ff55a792af5915e309cc949e3c
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-11 18:35:47 -0500

    completeiteration1

--- Emojional/Emojional/ViewController.swift
+++ Emojional/Emojional/ViewController.swift
@@ -14,7 +14,9 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController ()
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present (alertController, animated: true, completion: nil)
 }
-
+}
